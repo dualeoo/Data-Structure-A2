9c1d37e723ef7e953d5e30be1e5238dff6b5461e
Minh Tran Van <dualeoo@gmail.com>
2018-08-22 11:20:29 +0700

    addToMostRecentMessage: handle case where there is no chat room keepOldMessage: make sure when filter vulgarity, no need to update the old message

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
 const admin = require("firebase-admin");
-const functions = require("firebase-functions")
+const functions = require("firebase-functions");
 
 
 admin.initializeApp(functions.config().firebase);
 
-var db = admin.firestore();
-
+const db = admin.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+db.settings(settings);
 const changesRef = db.collection('changes');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -28,7 +29,7 @@ exports.filterVulgarity = functions.firestore
         const contentAfterFilter = contentBeforeFilter.replace(/fuck/gi, "");
         console.log(`Content after filter: ${contentAfterFilter}`);
 
-        changesRef.add(newValue)
+        changesRef.add(newValue);
 
         return snap.ref.update({content: contentAfterFilter});
 
@@ -39,20 +40,23 @@ exports.addToMostRecentMessage = functions.firestore
     .onCreate((snap, context) => {
         // const newValue = snap.ref.firestore;
         let newMessage = snap.data();
-        console.log(`The new message = ${newMessage}`);
+        console.log(`The new message ID = ${snap.id}`);
         const chatRoomRef = newMessage.chatRoom;
-        console.log(`The reference to chat room = ${chatRoomRef.id}`);
-        const mostRecentMessagesRef = chatRoomRef.collection("mostRecentMessages");
-        console.log(`The mostRecentMessages collection Ref = ${mostRecentMessagesRef.id}`);
-        let snapshotId = snap.id;
-        console.log(`The snapshot ID = ${snapshotId}`);
+        if (chatRoomRef) {
+            console.log(`The reference to chat room = ${chatRoomRef.id}`);
+            const mostRecentMessagesRef = chatRoomRef.collection("mostRecentMessages");
+            console.log(`The mostRecentMessages collection Ref = ${mostRecentMessagesRef.id}`);
+            let snapshotId = snap.id;
+            console.log(`The snapshot ID = ${snapshotId}`);
 
-        changesRef.add(newMessage)
+            changesRef.add(newMessage);
 
-        return mostRecentMessagesRef.doc(snapshotId).set(newMessage).then((result) => {
-            console.log(`Successfully add to mostRecentMessages at ${result.writeTime}`);
-            return 0;
-        });
+            return mostRecentMessagesRef.doc(snapshotId).set(newMessage).then((result) => {
+                console.log(`Successfully add to mostRecentMessages at ${result.writeTime}`);
+                return 0;
+            });
+        }
+        return 0;
         // return mostRecentMessagesRef.add(newMessage)
     });
 
@@ -61,27 +65,29 @@ exports.keepOldMessage = functions.firestore
     .onUpdate((change, context) => {
         let newDocumentSnapshot = change.after;
         let newDocumentReference = newDocumentSnapshot.ref;
-        console.log(`Path to Doc Ref = ${newDocumentReference.path}`);
+        console.log(`Path to Message = ${newDocumentReference.path}`);
         const newValue = newDocumentSnapshot.data();
         const previousValue = change.before.data();
 
         let oldContent = previousValue.content;
         let newContent = newValue.content;
 
-        if (newContent !== oldContent) {
+        if (newContent !== oldContent && oldContent.search(/fuck/gi) !== -1) {
             console.log(`Old content = ${oldContent}`);
             // let messagesRef = firestore.collection("messages");
             // let oldMessage = Object.assign({}, previousValue, {newMessage: newDocumentReference});
             console.log(`New content = ${newContent}`);
-            
-            changesRef.add(newContent)
-            
-            return newDocumentReference.collection("previousVersions")
-                .add({content: oldContent, time: previousValue.time})
-                .then(result => {
-                    console.log(`Path to backup message = ${documentReference.path}`);
-                    return 0;
-                });
+
+            changesRef.add(newValue);
+            if (previousValue.time && oldContent) {
+                return newDocumentReference.collection("previousVersions")
+                    .add({content: oldContent, time: previousValue.time})
+                    .then(result => {
+                        console.log(`Path to backup message = ${newDocumentReference.path}`);
+                        return 0;
+                    });
+            }
+            return 0;
         }
         return 0;
     });
@@ -92,8 +98,8 @@ exports.keepDeletedMessage = functions.firestore
         let firestore = snap.ref.firestore;
         let oldMessage = snap.data();
         let docRef = firestore.collection("deletedMessages").doc(snap.id);
-        
-        changesRef.add(oldMessage)
+
+        changesRef.add(oldMessage);
         
         return docRef.set(oldMessage).then(result => {
             console.log(`Successfully backup the message at ${result.writeTime.toDate().toLocaleString()}`);
@@ -106,8 +112,8 @@ exports.onPostContainerCreate = functions.firestore
     .onCreate(async (snap, context) => {
         console.log(`The postContainer recently created =  ${snap.id}`);
         let postContainer = snap.data();
-        
-        changesRef.add(postContainer)
+
+        changesRef.add(postContainer);
         
         let postContainerOwnerRef = postContainer.userId;
         let postContainerOwnerId = postContainerOwnerRef.id;
@@ -160,8 +166,8 @@ exports.onPostContainerLiked = functions.firestore
     .document('postContainers/{postContainerId}/likedBy/{userId}')
     .onCreate(async (snap, context) => {
         let userLike = snap.data();
-        
-        changesRef.add(userLike)
+
+        changesRef.add(userLike);
 
         let postContainerId = context.params.postContainerId;
         console.log(`User ${context.params.userId} liked postContainer ${postContainerId}`);
@@ -205,8 +211,8 @@ exports.onPostcontainerDeleted = functions.firestore
     .onDelete((snap, context) => {
         let firestore = snap.ref.firestore;
         let oldPostContainer = snap.data();
-        
-        changesRef.add(oldPostContainer)
+
+        changesRef.add(oldPostContainer);
         
         let docRef = firestore.collection("deletedPostContainer").doc(snap.id);
         return docRef.set(oldPostContainer).then(result => {
