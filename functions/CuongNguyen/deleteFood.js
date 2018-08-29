af26183f4391260514b558d643abfe4238a28f6f
NarutoChicken <37897292+NarutoChicken@users.noreply.github.com>
2018-08-29 23:20:12 +0700

    Basicually finish integrated cuong work into our works

---
+++ functions/CuongNguyen/deleteFood.js
@@ -0,0 +1,39 @@
+exports.handler = function (snap, context, db) {
+
+    //Delete Food Ranking
+    var deleteRanking = db
+        .collection('Food-Ranking')
+        .where('fid', '==', snap.data().fid)
+        .get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                var updateRanking = db
+                    .collection('Food-Ranking')
+                    .doc(doc.id)
+                    .update({Deleted: true});
+                console.log('Food ' + snap.data().fid + ' has deleted');
+                console.log('Food-Ranking from ' + snap.data().uid + 'Deleted: ' + snap.data().Deleted) //TODO
+            });
+            return 0;
+        });
+
+    // Create a list of changes made
+
+    var numOfChanges = 0;
+    var changeNum = db
+        .collection('ChangeList')
+        .get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                numOfChanges += 1;
+            });
+            var changeUpdate = db
+                .collection('ChangeList')
+                .doc('Change number' + numOfChanges)
+                .set(snap.data());
+            return 0;
+        });
+
+    var result = [deleteRanking, changeNum];
+    return result;
+};
--- functions/indexToan.js
+++ functions/indexToan.js
@@ -1,9 +1,6 @@
 const functions = require('firebase-functions');
 var admin = require('firebase-admin');
 
-
-admin.initializeApp(functions.config().firebase);
-
 var db = admin.firestore();
 
 const restaurantRef = db.collection('restaurants');
